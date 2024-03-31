e0fe804b5db34944df1bc736e068c2c4bcef9ac6
Paul Stivers <pstivers3@comcast.net>
2024-03-31 06:18:00 -0400

    updates

--- modules/func_exp.py
+++ modules/func_exp.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 def say_hi(first, last='Shmoe'):
     """Documentation: Say Hi.""" 
-    print ('\nHi {} {}!'.format(first, last))
+    print('\nHi {} {}!'.format(first, last))
 
 def return_test():
     return '\nreturn before printing other text'
-    print 'you should not see this'
+    print('you should not see this')
 
 def main():
     say_hi('Joe')
-    print return_test(), '\n'
+    print(return_test(), '\n')
 
 if __name__=='__main__':
     main()
--- modules/module_exp.py
+++ modules/module_exp.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 from func_exp import say_hi, return_test
 import func_exp
 
-print '\n', dir(func_exp)
+print('\n', dir(func_exp))
 
 say_hi('Gary')
 
-print return_test()
+print(return_test())
 
 func_exp.say_hi('Larry')
 print
