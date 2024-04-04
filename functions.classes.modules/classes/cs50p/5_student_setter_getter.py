55a66632466ec0a94f4a16b9fb211086c2240ea9
Paul Stivers <pstivers3@comcast.net>
2024-04-04 01:30:38 -0400

    changes

--- functions.classes.modules/classes/cs50p/5_student_setter_getter.py
+++ functions.classes.modules/classes/cs50p/5_student_setter_getter.py
@@ -8,39 +8,49 @@
 
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing Name")
         self.name     = name
-        self.house    = house  # will call setter method
+        self.house    = house  # will call setter method. If used _house, would circumvent error checking in setter
 
     def __str__(self):   # required in order to print strings from the Student object
         return f"{self.name} from {self.house}"
 
-    # Getter - must go through this function to get house
-    @property 
-    def house(self):
-        return self._house # must match method name in setter 
+    @property
+    def name(self):
+        return self._name
 
-    # Setter - must go through this function to set house
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name=name
+
+    @property # getter
+    def house(self):
+        return self._house # must match instance variable name in setter
+
+    # any call to .house will go though this function to set house
     @house.setter 
     def house(self, house):  # needs to be same method name as getter
         # error checking is moved into setter method 
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house") 
-        self._house = house # must be named differently than method in __init__
+        # instance variable name "_house" must be different that instance variable name "house" __init__.
+        # By convension use _
+        self._house = house
 
 def main():
     student = get_student()
     # This will be prevented by setter method in class Student.
     # student.house = "Number Four, Privet Drive" # overwrite house from get_student method, in student object
-    print(student)
 
+    # This overwrite though will still work. Honor system to not use _<name> except in getter and setter
+    # student._house = "Number Four, Privet Drive"
+    print(student)
 
 def get_student():
     name     = input("Name: ")
     house    = input("House: ")
     return Student(name, house) # constructor
-        
 
 if __name__ == "__main__":
     main()
